692c41a964f79ad97ac413cab37a5529f06fd824
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-17 11:57:45 -0500

    Dr-implementation

--- src/main/java/com/hookah/demo/service/DrinkImplementation.java
+++ src/main/java/com/hookah/demo/service/DrinkImplementation.java
@@ -1,21 +1,22 @@
 package com.hookah.demo.service;
 
 import com.hookah.demo.entity.Drinks;
-import com.hookah.demo.repository.HookahRepository;
+import com.hookah.demo.repository.DrinkRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 
 public class DrinkImplementation implements DrinkService {
-    private  final HookahRepository hookahRepository;
+   private  final DrinkRepository drinkRepository;
 
-    public DrinkImplementation(HookahRepository hookahRepository) {
-        this.hookahRepository = hookahRepository;
+    public DrinkImplementation(DrinkRepository drinkRepository) {
+        this.drinkRepository = drinkRepository;
     }
+
 
     @Override
     public List<Drinks> getdrinks() {
-        return null;
+        return  drinkRepository.findAll();
     }
 }

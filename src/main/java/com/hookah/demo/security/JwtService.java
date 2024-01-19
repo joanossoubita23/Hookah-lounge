a3a6cf658bf4e6076e1b190ceda02a8a1ab7e413
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-18 21:13:04 -0500

    Jwst

--- src/main/java/com/hookah/demo/security/JwtService.java
+++ src/main/java/com/hookah/demo/security/JwtService.java
@@ -1,4 +1,43 @@
 package com.hookah.demo.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
 public class JwtService {
+    //Create token time
+    static final long EXPIRATIONTIME = 86400000;
+    static final String PREFIX = "Bearer";
+    static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String getToken(String username) {
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                .signWith(key)
+                .compact();
+        return token;
+    }
+
+    public String getAuthUser(HttpServletRequest request) {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (token != null) {
+            String user = Jwts.parser()
+                    .verifyWith((SecretKey) key)
+                    .build()
+                    .parseSignedClaims(token.replace(PREFIX, ""))
+                    .getPayload()
+                    .getSubject();
+            if (user != null) return user;
+        }
+        return null;
+    }
+
 }

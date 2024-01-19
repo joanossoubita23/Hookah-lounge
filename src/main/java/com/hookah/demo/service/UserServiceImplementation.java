6c7172b05175c916d591849705eb932a72037f60
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-18 20:01:52 -0500

    UserImplementation

---
+++ src/main/java/com/hookah/demo/service/UserServiceImplementation.java
@@ -0,0 +1,22 @@
+package com.hookah.demo.service;
+
+import com.hookah.demo.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserServiceImplementation implements UserDetailsService {
+    private  final UserRepository userRepository;
+
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}

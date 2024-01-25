77f8695f36e8df9b6b5ec92f739eb0bbda44c541
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-24 22:17:27 -0500

    end project

--- src/main/java/com/hookah/demo/Web/LoginController.java
+++ src/main/java/com/hookah/demo/Web/LoginController.java
@@ -1,4 +1,32 @@
 package com.hookah.demo.Web;
 
+import com.hookah.demo.Pojo.AccountCredentials;
+import com.hookah.demo.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class LoginController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+    @PostMapping("/login")
+    public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials){
+        UsernamePasswordAuthenticationToken creds= new UsernamePasswordAuthenticationToken(credentials.getUsername(),credentials.getPassword());
+        Authentication auth=authenticationManager.authenticate(creds);
+        String jwts=jwtService.getToken(auth.getName());
+        return  ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION,"Bearer"+jwts)
+                .header(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,"AUTHORIZATION")
+                .build();
+    }
 }
+
